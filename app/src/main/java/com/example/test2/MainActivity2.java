78d18a245f8a42e158ff97e6f4d2107d4be4410d
Loh Wei Ru Natalie <s10243551@connect.np.edu.sg>
2023-05-08 11:28:53 +0800

    week2 done

--- app/src/main/java/com/example/test2/MainActivity2.java
+++ app/src/main/java/com/example/test2/MainActivity2.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.week2new.R;
+
 public class MainActivity2 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
